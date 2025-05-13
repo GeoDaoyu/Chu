ddbca39726b4fadba242e0ad57baff0c4a367e8e
zhanggy <zhanggy@geoscene.cn>
2025-05-13 16:01:59 +0800

    refactor: eslint9
    弃用eslint-config-xo

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,8 @@
-import { includeIgnoreFile } from '@eslint/compat';
-import xoReactSpace from 'eslint-config-xo-react/space';
-import xoSpaceBrowser from 'eslint-config-xo/space/browser';
-import { defineConfig } from 'eslint/config';
-import { fileURLToPath } from 'node:url';
-
-const gitignorePath = fileURLToPath(new URL('.gitignore', import.meta.url));
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import js from "@eslint/js";
 
 export default defineConfig([
-  includeIgnoreFile(gitignorePath),
-  ...xoSpaceBrowser,
-  ...xoReactSpace,
-  {
-    rules: {
-      '@stylistic/object-curly-spacing': ['error', 'always'], // 强制 对象字面量（{}）内部必须包含空格
-      '@stylistic/jsx-quotes': ['error', 'prefer-double'], // 强制 JSX 属性使用双引号
-    },
-  },
+	{ files: ["**/*.js"], languageOptions: { globals: globals.browser } },
+	{ files: ["**/*.js"], plugins: { js }, extends: ["js/recommended"] },
 ]);
--- prettier.config.js
+++ prettier.config.js
@@ -1,9 +1,6 @@
-module.exports = {
+export default {
   pluginSearchDirs: false,
-  plugins: [
-    require.resolve('prettier-plugin-organize-imports'),
-    require.resolve('prettier-plugin-packagejson'),
-  ],
+  plugins: ['prettier-plugin-organize-imports', 'prettier-plugin-packagejson'],
   printWidth: 80,
   proseWrap: 'never',
   singleQuote: true,
