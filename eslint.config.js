69c03b1e51c7f8156b27390176ad10c4315a1581
zhanggy <zhanggy@geoscene.cn>
2025-05-28 10:31:03 +0800

    chore: using antfu/eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,10 @@
-import { defineConfig } from "eslint/config";
-import globals from "globals";
-import js from "@eslint/js";
+import antfu from '@antfu/eslint-config';
 
-export default defineConfig([
-	{ files: ["**/*.js"], languageOptions: { globals: globals.browser } },
-	{ files: ["**/*.js"], plugins: { js }, extends: ["js/recommended"] },
-]);
+export default antfu({
+  vue: false,
+  react: true,
+  stylistic: {
+    semi: true,
+  },
+  ignores: ['apps/*/public/assets'],
+});
