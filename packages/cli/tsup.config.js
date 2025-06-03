0c4dee0684965d826f9cc4c0d9f82a222ff7cc79
zhanggy <zhanggy@geoscene.cn>
2025-06-03 14:03:06 +0800

    fix(cli): build cjs

--- packages/cli/tsup.config.js
+++ packages/cli/tsup.config.js
@@ -3,11 +3,11 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   entry: ['src/index.js'],
   outDir: 'dist',
-  format: 'esm',
+  format: 'cjs',
   target: 'node20.18',
   bundle: true,
   minify: true,
-  external: ['@inquirer/prompts', 'chalk', 'commander'],
+  external: [],
   banner: {
     js: '#!/usr/bin/env node',
   },
