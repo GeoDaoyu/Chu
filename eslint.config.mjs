b11aa63b8207a2c678341c87655e3f5337d4dd19
zhanggy <zhanggy@geoscene.cn>
2025-06-26 10:35:04 +0800

    refactor: eslint-config-ali

---
+++ eslint.config.mjs
@@ -0,0 +1,18 @@
+import { react } from 'eslint-config-ali';
+import prettier from 'eslint-plugin-prettier/recommended';
+
+export default [
+  ...react,
+  prettier,
+  {
+    ignores: [
+      'apps/*/public/assets',
+      'apps/*/src/.umi',
+      'apps/*/src/.umi-production',
+      'packages/*/.dumi',
+      'apps/*/dist',
+      'packages/*/dist',
+      'packages/*/bin',
+    ],
+  },
+];
