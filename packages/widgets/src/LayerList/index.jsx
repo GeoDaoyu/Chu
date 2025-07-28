6fe909f4f121c257829d292b255175ac83438982
zhanggy <zhanggy@geoscene.cn>
2025-07-28 09:42:07 +0800

    refactor: arcgis-layer-list

--- packages/widgets/src/LayerList/index.jsx
+++ packages/widgets/src/LayerList/index.jsx
@@ -1,26 +1,7 @@
-import LayerList from '@arcgis/core/widgets/LayerList';
-import { useEffect, useRef } from 'react';
+import '@arcgis/map-components/components/arcgis-layer-list';
 
-const Widget = ({ view }) => {
-  const ref = useRef();
-  const widgetRef = useRef();
-
-  useEffect(() => {
-    if (ref.current) {
-      widgetRef.current = new LayerList({
-        view,
-        container: ref.current,
-      });
-    }
-
-    return () => {
-      if (widgetRef.current) {
-        widgetRef.current.destroy();
-      }
-    };
-  }, [view]);
-
-  return <div ref={ref} />;
+const Widget = () => {
+  return <arcgis-layer-list reference-element="view" />;
 };
 
 export default Widget;
