3abf93b0418d817bc7de29da01cddf8770618306
zhanggy <zhanggy@geoscene.cn>
2025-07-28 09:39:43 +0800

    refactor: arcgis-legend

--- packages/widgets/src/Legend/index.jsx
+++ packages/widgets/src/Legend/index.jsx
@@ -1,26 +1,7 @@
-import Legend from '@arcgis/core/widgets/Legend';
-import { useEffect, useRef } from 'react';
+import '@arcgis/map-components/components/arcgis-legend';
 
-const Widget = ({ view }) => {
-  const ref = useRef();
-  const widgetRef = useRef();
-
-  useEffect(() => {
-    if (ref.current) {
-      widgetRef.current = new Legend({
-        view,
-        container: ref.current,
-      });
-    }
-
-    return () => {
-      if (widgetRef.current) {
-        widgetRef.current.destroy();
-      }
-    };
-  }, [view]);
-
-  return <div ref={ref} />;
+const Widget = () => {
+  return <arcgis-legend reference-element="view" />;
 };
 
 export default Widget;
