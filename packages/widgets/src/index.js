ca60fa34ae91de3004b2f8714f7c488712251de7
GeoDaoyu <geodaoyu@foxmail.com>
2025-08-13 09:46:57 +0800

    chore(widgets): clean export

--- packages/widgets/src/index.js
+++ packages/widgets/src/index.js
@@ -1,5 +1,3 @@
 export { default as LayerList } from './LayerList';
 export * from './LayerTree';
 export { default as Legend } from './Legend';
-export { default as MediaMixin } from './MediaMixin';
-export { default as Weather } from './Weather';
