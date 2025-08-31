063f6a23f79c7dcd7e19d50eb3c559ad770cf39d
zhanggy <geodaoyu@foxmail.com>
2025-08-31 15:49:41 +0800

    feat: add Area Measurement 2D & Distance Measurement 2D

--- apps/map-pro/src/pages/Analysis/config.js
+++ apps/map-pro/src/pages/Analysis/config.js
@@ -1,2 +1,25 @@
-const dataSource = [];
+import {
+  LineOutlined,
+  BorderOutlined,
+} from '@ant-design/icons';
+import DirectLineMeasurement2D from '@chu/widgets/DirectLineMeasurement2D';
+import AreaMeasurement2D from '@chu/widgets/AreaMeasurement2D';
+
+const dataSource = [
+  {
+    key: 'DirectLineMeasurement2D',
+    title: '距离测量',
+    avatar: <LineOutlined />,
+    description: '我是距离测量',
+    component: <DirectLineMeasurement2D />,
+  },
+  {
+    key: 'AreaMeasurement2D',
+    title: '面积测量',
+    avatar: <BorderOutlined />,
+    description: '我是面积测量',
+    component: <AreaMeasurement2D />,
+  },
+];
+
 export default dataSource;
---
+++ packages/widgets/src/AreaMeasurement2D/index.md
@@ -0,0 +1,10 @@
+# AreaMeasurement2D
+
+Usage:
+
+```jsx
+import AreaMeasurement2D from '@chu/widgets/AreaMeasurement2D';
+
+const App = () => <AreaMeasurement2D />;
+export default App;
+```
