30a3ec5970ab1e573e23e2b85d92aa37d7bf9c5c
GeoDaoyu <geodaoyu@foxmail.com>
2025-09-02 10:08:10 +0800

    feat(widgets): add ScaleBar

---
+++ packages/widgets/src/ScaleBar/index.jsx
@@ -0,0 +1,7 @@
+import '@arcgis/map-components/components/arcgis-scale-bar';
+
+const Widget = () => {
+  return <arcgis-scale-bar reference-element="view" />;
+};
+
+export default Widget;
---
+++ packages/widgets/src/ScaleBar/index.md
@@ -0,0 +1,10 @@
+# ScaleBar
+
+Usage:
+
+```jsx
+import ScaleBar from '@chu/widgets/ScaleBar';
+
+const App = () => <ScaleBar />;
+export default App;
+```
