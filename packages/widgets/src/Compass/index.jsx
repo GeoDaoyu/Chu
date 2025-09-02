1098b43275ff3aaa056011aa7c1b4653f68b06c6
GeoDaoyu <geodaoyu@foxmail.com>
2025-09-02 10:07:58 +0800

    feat(widgets): add Compass

---
+++ packages/widgets/src/Compass/index.jsx
@@ -0,0 +1,7 @@
+import '@arcgis/map-components/components/arcgis-compass';
+
+const Widget = () => {
+  return <arcgis-compass reference-element="view" />;
+};
+
+export default Widget;
---
+++ packages/widgets/src/Compass/index.md
@@ -0,0 +1,10 @@
+# Compass
+
+Usage:
+
+```jsx
+import Compass from '@chu/widgets/Compass';
+
+const App = () => <Compass />;
+export default App;
+```
