d625d915f609fbbfe64fe67d7947a5f11b28343f
zhanggy <geodaoyu@foxmail.com>
2025-09-06 16:52:17 +0800

    feat(widgets): add Search

---
+++ packages/widgets/src/Search/index.jsx
@@ -0,0 +1,7 @@
+import "@arcgis/map-components/components/arcgis-search";
+
+const Widget = () => {
+  return <arcgis-search reference-element="view" />;
+};
+
+export default Widget;
---
+++ packages/widgets/src/Search/index.md
@@ -0,0 +1,10 @@
+# Search
+
+Usage:
+
+```jsx
+import Search from '@chu/widgets/Search';
+
+const App = () => <Search />;
+export default App;
+```
