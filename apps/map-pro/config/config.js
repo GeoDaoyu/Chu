f8bb47721e1f508c590066d82155c3ca46ca3eb5
GeoDaoyu <geodaoyu@foxmail.com>
2025-09-18 09:03:47 +0800

    chore: fix HtmlElement bug

--- apps/map-pro/config/config.js
+++ apps/map-pro/config/config.js
@@ -19,6 +19,7 @@ export default defineConfig({
   jsMinifier: 'terser',
   publicPath: process.env.NODE_ENV === 'development' ? '/' : './',
   headScripts: [
+    './config/fixHTMLElement.js',
     {
       src: UMI_ENV ? `./config/index.${UMI_ENV}.js` : './config/index.js',
     },
