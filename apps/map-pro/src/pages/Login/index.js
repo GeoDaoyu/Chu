69f9d4b6e309d506feb57fcc2ac96274aabc7ba6
GeoDaoyu <geodaoyu@foxmail.com>
2025-12-14 19:16:06 +0800

    style: improve Login page with semantic class names and separated styles

--- apps/map-pro/src/pages/Login/index.js
+++ apps/map-pro/src/pages/Login/index.js
@@ -1,8 +1,8 @@
 import { history, useModel } from '@umijs/max';
 import { LockOutlined, UserOutlined } from '@ant-design/icons';
+import { Button, Card, Checkbox, Form, Input } from 'antd';
 import { setAuthority } from '@/utils/authority';
 import { getRole, login } from './service.js';
-import { LoginForm, ProFormCheckbox, ProFormText } from '@ant-design/pro-components';
 import styles from './index.less';
 
 const Login = () => {
@@ -24,70 +24,80 @@ const Login = () => {
     // 跳转回home页面
     history.push('/home');
   };
+
+  const [form] = Form.useForm();
+
   return (
     <div className={styles.container}>
-      <LoginForm
-        contentStyle={{
-          minWidth: 280,
-          maxWidth: '75vw',
-        }}
-        logo={<img alt="logo" src="/logo.svg" />}
-        title="Chu"
-        subTitle="现代的WebGIS框架"
-        initialValues={{
-          autoLogin: true,
-        }}
-        onFinish={async (values) => {
-          await handleSubmit(values);
-        }}
-      >
-        <>
-          <ProFormText
-            name="username"
-            fieldProps={{
-              size: 'large',
-              prefix: <UserOutlined />,
-            }}
-            placeholder="用户名: admin or user"
-            rules={[
-              {
-                required: true,
-                message: '请输入用户名!',
-              },
-            ]}
-          />
-          <ProFormText.Password
-            name="password"
-            fieldProps={{
-              size: 'large',
-              prefix: <LockOutlined />,
-            }}
-            placeholder="密码: （无密码）"
-            rules={[
-              {
-                required: true,
-                message: '请输入密码！',
-              },
-            ]}
-          />
-        </>
-        <div
-          style={{
-            marginBottom: 24,
-          }}
+      <div className={styles.loginWrapper}>
+        <Card
+          className={styles.loginCard}
+          title={
+            <div className={styles.loginLogo}>
+              <img alt="logo" src="/logo.svg" className={styles.loginLogoImg} />
+              <span>Chu</span>
+            </div>
+          }
+          extra={
+            <div className={styles.loginSubtitle}>
+              <div>现代的WebGIS框架</div>
+            </div>
+          }
         >
-          <ProFormCheckbox noStyle name="autoLogin">
-            自动登录
-          </ProFormCheckbox>
-          <a
-            style={{
-              float: 'right',
+          <Form
+            form={form}
+            initialValues={{
+              autoLogin: true,
+            }}
+            onFinish={async (values) => {
+              await handleSubmit(values);
             }}
           >
-            忘记密码
-          </a>
-        </div>
-      </LoginForm>
+            <Form.Item
+              name="username"
+              rules={[
+                {
+                  required: true,
+                  message: '请输入用户名!',
+                },
+              ]}
+            >
+              <Input size="large" prefix={<UserOutlined />} placeholder="用户名: admin or user" />
+            </Form.Item>
+            <Form.Item
+              name="password"
+              rules={[
+                {
+                  required: true,
+                  message: '请输入密码！',
+                },
+              ]}
+            >
+              <Input.Password
+                size="large"
+                prefix={<LockOutlined />}
+                placeholder="密码: （无密码）"
+              />
+            </Form.Item>
+            <div className={styles.loginFormItem}>
+              <Form.Item name="autoLogin" valuePropName="checked" noStyle>
+                <Checkbox>自动登录</Checkbox>
+              </Form.Item>
+              <a className={styles.forgotPassword}>忘记密码</a>
+            </div>
+            <Form.Item>
+              <Button
+                type="primary"
+                htmlType="submit"
+                size="large"
+                className={styles.loginFormButton}
+              >
+                登录
+              </Button>
+            </Form.Item>
+          </Form>
+        </Card>
+      </div>
     </div>
   );
 };
