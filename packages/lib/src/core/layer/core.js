0dcd8ecad24d4dcd785200cd5ded8809c63303d7
GeoDaoyu <geodaoyu@foxmail.com>
2025-12-18 18:11:59 +0800

    feat: support groupLayer

--- packages/lib/src/core/layer/core.js
+++ packages/lib/src/core/layer/core.js
@@ -1,6 +1,4 @@
-import Layer from '@arcgis/core/layers/Layer';
 import createLayer from './createLayer';
-import { cond, has, T } from 'ramda';
 
 export const hasLayer = (view, id) => {
   const layer = view.map.findLayerById(id);
@@ -8,15 +6,7 @@ export const hasLayer = (view, id) => {
 };
 
 export const addLayer = async (view, layerInfo) => {
-  if (!layerInfo) {
-    return;
-  }
-  const { key, url, type, ...rest } = layerInfo;
-  const layer = cond([
-    [has('type'), () => createLayer({ id: key, ...layerInfo })],
-    [has('url'), () => Layer.fromArcGISServerUrl({ url, properties: { id: key, ...rest } })],
-    [T, () => new Layer({ id: key, ...rest })],
-  ])(layerInfo);
+  const layer = await createLayer(layerInfo);
   view.map.add(layer);
 };
 
--- packages/lib/src/core/layer/createLayer.js
+++ packages/lib/src/core/layer/createLayer.js
@@ -1,3 +1,4 @@
+import Layer from '@arcgis/core/layers/Layer';
 import FeatureLayer from '@arcgis/core/layers/FeatureLayer';
 import GeoJSONLayer from '@arcgis/core/layers/GeoJSONLayer';
 import GraphicsLayer from '@arcgis/core/layers/GraphicsLayer';
@@ -7,6 +8,8 @@ import IntegratedMesh3DTilesLayer from '@arcgis/core/layers/IntegratedMesh3DTile
 import MapImageLayer from '@arcgis/core/layers/MapImageLayer';
 import TileLayer from '@arcgis/core/layers/TileLayer';
 import VectorTileLayer from '@arcgis/core/layers/VectorTileLayer';
+import GroupLayer from '@arcgis/core/layers/GroupLayer';
+import { cond, has, T } from 'ramda';
 
 const supportLayerMap = new Map([
   ['feature', FeatureLayer],
@@ -20,9 +23,31 @@ const supportLayerMap = new Map([
   ['vector-tile', VectorTileLayer],
 ]);
 
+const createLayerByUrl = async ({ key, url, ...rest }) =>
+  await Layer.fromArcGISServerUrl({ url, properties: { id: key, ...rest } });
+
 // type是只读字段，所以不能直接new Layer
-const createLayer = ({ type, ...rest }) => {
+const createLayerByType = ({ type, key, ...rest }) => {
+  if (type === 'group') {
+    return createGroupLayer(rest);
+  }
   const LayerClass = supportLayerMap.get(type);
-  return new LayerClass(rest);
+  return new LayerClass({ id: key, ...rest });
 };
+
+const createGroupLayer = async ({ layers: layerInfos, key, ...rest }) => {
+  const layers = await Promise.all(layerInfos.map(createLayer));
+  return new GroupLayer({
+    ...rest,
+    id: key,
+    layers,
+  });
+};
+
+const createLayer = cond([
+  [has('type'), createLayerByType],
+  [has('url'), createLayerByUrl],
+  [T, () => undefined],
+]);
+
 export default createLayer;
