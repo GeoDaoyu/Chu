c90a2378feadebb05b64c7d1547bb577ca79a3f3
GeoDaoyu <geodaoyu@foxmail.com>
2026-03-25 10:27:00 +0800

    feat: layerList

--- packages/widgets/src/LayerList/index.jsx
+++ packages/widgets/src/LayerList/index.jsx
@@ -1,7 +1,58 @@
-import '@arcgis/map-components/components/arcgis-layer-list';
+import { watch } from '@arcgis/core/core/reactiveUtils';
+import { useViewStore } from '@chu/store';
+import useLayerTreeStore from '@chu/store/useLayerTreeStore';
+import { List, Typography, Switch, Tooltip } from 'antd';
+import { useEffect, useState } from 'react';
+import styles from './index.less';
+import { Space } from 'antd';
+import { CloseOutlined } from '@ant-design/icons';
 
-const Widget = () => {
-  return <arcgis-layer-list reference-element="view" />;
+const LayerList = () => {
+  const view = useViewStore((state) => state.view);
+  const [dataSource, setDataSource] = useState(view.map.layers.toArray());
+  const { checkedKeys, setCheckedKeys } = useLayerTreeStore();
+
+  useEffect(() => {
+    watch(
+      () => view.map.layers.length,
+      () => {
+        setDataSource(view.map.layers.toArray());
+      },
+    );
+  }, [view]);
+
+  const visibleSwitch = (id, checked) => {
+    const layer = view.map.findLayerById(id);
+    layer.visible = checked;
+  };
+
+  const removeLayer = (id) => {
+    setCheckedKeys(checkedKeys.filter((v) => v !== id));
+  };
+
+  return (
+    <List
+      className={styles.list}
+      dataSource={dataSource}
+      renderItem={(item) => (
+        <List.Item
+          extra={
+            <Space>
+              <Tooltip title="移除">
+                <CloseOutlined onClick={() => removeLayer(item.id)} />
+              </Tooltip>
+              <Switch
+                defaultChecked={item.visible}
+                onChange={(checked) => visibleSwitch(item.id, checked)}
+              />
+            </Space>
+          }
+        >
+          <Typography.Text>{item.title}</Typography.Text>
+        </List.Item>
+      )}
+    />
+  );
 };
 
-export default Widget;
+export default LayerList;
