9336a8a5178d43236267f1e4d942c485ee5d4dae
GeoDaoyu <geodaoyu@foxmail.com>
2026-08-22 10:31:43 +0800

    refactor(ui): 将 FunctionList 的 antd List 迁移至 Listy

--- packages/ui/src/FunctionList/components/List/index.jsx
+++ packages/ui/src/FunctionList/components/List/index.jsx
@@ -1,30 +1,34 @@
 import { RightOutlined } from '@ant-design/icons';
-import { Button, List } from 'antd';
+import { Button, Listy } from 'antd';
 import styles from './index.less';
 
 const FunctionList = ({ dataSource, goTo }) => {
   return (
-    <List
+    <Listy
       className={styles.list}
-      itemLayout="horizontal"
-      dataSource={dataSource}
-      renderItem={({ avatar, title, description, component }) => (
-        <List.Item
-          actions={[
-            <Button
-              key="link"
-              type="link"
-              icon={<RightOutlined />}
-              iconPosition="end"
-              disabled={!component}
-              onClick={() => goTo(component)}
-            >
-              立即使用
-            </Button>,
-          ]}
-        >
-          <List.Item.Meta avatar={avatar} title={title} description={description} />
-        </List.Item>
+      height="calc(100vh - 240px)"
+      virtual={false}
+      items={dataSource}
+      rowKey="key"
+      itemRender={({ avatar, title, description, component }) => (
+        <div className={styles.item}>
+          <div className={styles.meta}>
+            <div className={styles.avatar}>{avatar}</div>
+            <div className={styles.content}>
+              <div className={styles.title}>{title}</div>
+              {description && <div className={styles.description}>{description}</div>}
+            </div>
+          </div>
+          <Button
+            type="link"
+            icon={<RightOutlined />}
+            iconPosition="end"
+            disabled={!component}
+            onClick={() => goTo(component)}
+          >
+            立即使用
+          </Button>
+        </div>
       )}
     />
   );
